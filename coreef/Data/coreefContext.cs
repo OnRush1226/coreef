990d1f96bb6f18d095f0cb37f258cef748784b70
whchenghu <915185290@qq.com>
2019-06-01 18:02:48 +0800

    update

---
+++ coreef/Data/coreefContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace coreef.Models
+{
+    public class coreefContext : DbContext
+    {
+        public coreefContext (DbContextOptions<coreefContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<coreef.Models.Phone> Phone { get; set; }
+    }
+}
--- coreef/Startup.cs
+++ coreef/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using coreef.Models;
 
 namespace coreef
 {
@@ -33,6 +35,9 @@ namespace coreef
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<coreefContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("coreefContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
